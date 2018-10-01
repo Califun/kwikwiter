23776b3513cefff60e7392eed59597b29a2e3baa
Armand Gonthier <armdgnthr@gmail.com>
2018-10-01 21:17:16 +0200

    - Added dynamic printing of user's name/surname/username - Added publication date

--- connection/models.py
+++ connection/models.py
@@ -3,4 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-	tweet_counter = models.PositiveIntegerField(default=0)
+    tweet_counter = models.PositiveIntegerField(default=0)
+    like_counter = models.PositiveIntegerField(default=0)
+    comment_counter = models.PositiveIntegerField(default=0)
